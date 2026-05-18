7548cf927ca98965aa13c091f03237a1ab98b372
Henry Aspeti <henry.aspeti@gmail.com>
2026-05-18 19:01:04 -0400

    nombre app

--- dashboard-app/config.py
+++ dashboard-app/config.py
@@ -25,7 +25,7 @@ CSRF_ENABLED = True
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "SkyDash UAB"
+APP_NAME = "Virtual Store UAB"
 
 # Uncomment to setup Setup an App icon
 # APP_ICON = "static/img/logo.jpg"
